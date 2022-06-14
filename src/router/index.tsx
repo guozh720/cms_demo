cb8c460efa6c53a52bf91724877202c312c8384b
guozh720 <1041674298@qq.com>
2022-06-14 22:22:58 +0800

    router Outlet and lazy Suspense try

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,46 @@
+import { lazy, Suspense } from "react";
+
 import App from "App";
-import List from "pages/List";
-import Login from "Login";
-import Register from "Register";
-import Edit from "pages/Edit";
-import Information from "pages/Information";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const routeArr = [
+    {
+        path: "/", component: App, children: [
+            { path: "information", component: lazy(() => import("pages/Information")) },
+            { path: "edit", component: lazy(() => import("pages/Edit")) },
+            { path: "list", component: lazy(() => import("pages/List")) },
+        ],
+    },
+    { path: "/login", component: lazy(() => import("Login")) },
+    { path: "/register", component: lazy(() => import("Register")) },
+
+];
 
 const MyRouter = () => (
     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<App/>}>
+        <Suspense fallback={<div>loading...</div>}>
+            <Routes>
+                {
+                    routeArr.map((item, index) => (
+                        // <Route path={item.path} element={<item.component />}></Route>
+                        item.children ?
+                            //有子路由
+                            <Route key={index} path={item.path} element={<item.component />}>
+                                {
+                                    item.children.map((e, i) => (
+                                        <Route key={i} path={e.path} element={<e.component />}></Route>
+                                    ))
+                                }
+                            </Route>
 
-            <Route path="/list" element={<List/>}></Route>
-            <Route path="/edit" element={ <Edit/>}></Route>
-            <Route path="/information" element={ <Information/>}></Route>
-        </Route>
+                            :
+                            //没子路由
+                            <Route key={index} path={item.path} element={<item.component />}></Route>
+                    ))
+                }
 
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-       
-    </Routes>
-
-
-
+            </Routes>
+        </Suspense>
     </BrowserRouter>
 )
 
